08ba0852ec49ee74df0f1038658bfe969f09a4b7
zlowram <zlowram@gmail.com>
2014-06-19 22:00:03 +0200

    Minor changes.

--- _test/testcmd.go
+++ _test/testcmd.go
@@ -16,7 +16,7 @@ type testCmd struct {
 	str string
 	cli.Command
 
-    flag1 *string
+	flag1 *string
 }
 
 func (tc *testCmd) Run() error {
@@ -32,19 +32,20 @@ func main() {
 		"Short description for this cli",
 		flag.Args(),
 	)
-    cmd := testCmd{
+	cmd := testCmd{
 		str: "a very long and imaginative string",
 		Command: cli.Command{
 			Name:        "subcmd1",
 			ShortName:   "s1",
 			Description: "short description of the subcmd1",
 			UsageLine:   "Usage line for subcmd1"},
+		//flag1: Command.Flag.String("f", "default", "string flag"),
 	}
-    cmd.flag1 = cmd.Flag.String("f", "default", "string flag")
-    c.AddCmd(&cmd)
-    c.AddCmd(&testCmd{
-        str: "a very long and imaginative string 2",
-        Command: cli.Command{
+	cmd.flag1 = cmd.Flag.String("f", "default", "string flag")
+	c.AddCmd(&cmd)
+	c.AddCmd(&testCmd{
+		str: "a very long and imaginative string 2",
+		Command: cli.Command{
 			Name:        "subcmd2",
 			ShortName:   "s2",
 			Description: "short description of the subcmd2",
